c6f59fedd26ebd0a15b9f617945d6351a4484491
Efendo <efe_mustafa.oener@icloud.com>
2023-06-12 21:05:21 +0200

    initialized repo

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#define print std::cout<<
+#define input std::cin>>
+#define str std::string
+#define pointer int*
+#define clr_text system("clear")
+
+/*
+
+* Template made by Efendo to make it a bit easier to code in C++ *
+                   ^^^^^^
+https://github.com/Efendo/
+
+*/
+
+int main()
+{
+    return 0; //EXIT_SUCCESS
+}
